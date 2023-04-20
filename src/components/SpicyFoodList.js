bb6b1faf82b182b76c92a75948fbc2edbb1ada9f
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-04-20 23:39:09 +0300

    done

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
-function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+function SpicyFoodList(){
+const [foods, setFoods] = useState(spicyFoods);
+const [filterBy, setFilterBy] = useState("All");
 
-  function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+const foodsToDisplay = foods.filter((food) => {
+  if (filterBy === "All") {
+    return true;
+  } else {
+    return food.cuisine === filterBy;
   }
+});
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
 
-  return (
-    <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
-    </div>
-  );
+const foodList = foodsToDisplay.map((food) => (
+  <li key={food.id} onClick={() => handleLiClick(food.id)}>
+    {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  </li>
+));
 }
 
 export default SpicyFoodList;
